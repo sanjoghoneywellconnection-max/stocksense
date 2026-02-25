61dc598ae0aaf4406e0f6a9193e08b1b5ec7fa1c
Sanjog <sanjog.honeywellconnection@gmail.com>
2026-02-25 15:21:13 +0530

    Session 10 - Portfolio Analysis complete

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SalesEntry from './pages/SalesEntry'
 import ComingSoon from './pages/placeholders/ComingSoon'
 import SkuExplorer from './pages/SkuExplorer'
 import ReorderPlanner from './pages/ReorderPlanner'
+import PortfolioAnalysis from './pages/PortfolioAnalysis'
 
 function Spinner() {
   return (
@@ -49,7 +50,7 @@ function AppRoutes() {
       <Route path="/sales" element={<OrgRoute><SalesEntry /></OrgRoute>} />
       <Route path="/skus" element={<OrgRoute><SkuExplorer /></OrgRoute>} />
       <Route path="/reorder" element={<OrgRoute><ReorderPlanner /></OrgRoute>} />
-      <Route path="/bcg" element={<OrgRoute><ComingSoon title="BCG Matrix" /></OrgRoute>} />
+      <Route path="/bcg" element={<OrgRoute><PortfolioAnalysis /></OrgRoute>} />
       <Route path="/trends" element={<OrgRoute><ComingSoon title="Inventory Trends" /></OrgRoute>} />
       <Route path="/warehouses-map" element={<OrgRoute><ComingSoon title="Warehouse Map" /></OrgRoute>} />
       <Route path="*" element={<Navigate to="/dashboard" />} />
